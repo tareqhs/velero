e0854546ddd098e1f5c0467efc4ca90267bdaf7b
Tareq Sharafy <tareq.sha@gmail.com>
2020-04-15 11:37:35 +0300

    pluralize flag name

--- pkg/cmd/server/server.go
+++ pkg/cmd/server/server.go
@@ -208,7 +208,7 @@ func NewCommand(f client.Factory) *cobra.Command {
 	command.Flags().DurationVar(&config.resourceTerminatingTimeout, "terminating-resource-timeout", config.resourceTerminatingTimeout, "how long to wait on persistent volumes and namespaces to terminate during a restore before timing out")
 	command.Flags().DurationVar(&config.defaultBackupTTL, "default-backup-ttl", config.defaultBackupTTL, "how long to wait by default before backups can be garbage collected")
 	command.Flags().DurationVar(&config.defaultResticMaintenanceFrequency, "default-restic-prune-frequency", config.defaultResticMaintenanceFrequency, "how often 'restic prune' is run for restic repositories by default")
-	command.Flags().StringSliceVar(&config.ignoredScheduleLabels, "ignore-schedule-label", config.ignoredScheduleLabels, "labels in Schedule resource to not copy to the Backup resources")
+	command.Flags().StringSliceVar(&config.ignoredScheduleLabels, "ignore-schedule-labels", config.ignoredScheduleLabels, "labels in Schedule resource to not copy to the Backup resources")
 
 	return command
 }
